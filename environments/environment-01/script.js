9b80bb4c1fbd53cac6711ddfb43ae72f0ef588d5
YawHB <yawhboateng@gmail.com>
2023-04-29 11:47:35 +0200

    Solved: Exercise 1
    I solved it two ways.
    
    first with a forEach loop
    second with a for of loop

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,4 +1,51 @@
 "use strict";
 window.addEventListener("load", start);
 
-async function createPost() {}
+//Del 1
+async function start() {
+  const userArray = await getData();
+  // userArray.forEach(showUsers);
+  // showUsers2(userArray);
+}
+
+async function getData() {
+  const response = await fetch("users.json");
+  const data = await response.json();
+  return data;
+}
+
+/* ========================================= ForEach metode ================================================================================== */
+//Del 2.A + 3.A
+function showUsers(user) {
+  if (user.role === "admin") {
+    const html = /*html*/ `
+ <li> ${user.name} - ${user.active}</li>
+ <li>${user.role}</li>
+ `;
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+/* ========================================= For of metode ================================================================================== */
+
+//Del 2.B + 3.B
+
+function showUsers2(array) {
+  for (const user of array) {
+    showUser2(user);
+  }
+}
+
+function showUser2(user) {
+  if (user.role === "admin") {
+    const html = /*html*/ `
+ <li> ${user.name} - ${user.active}</li>
+ <li>${user.role}</li>
+ <li>"Hej"</li>
+
+ `;
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+  }
+}
