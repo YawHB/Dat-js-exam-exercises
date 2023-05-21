ca8feb655cc598b8bc106a0b0da1af523295ad5b
YawHB <yawhboateng@gmail.com>
2023-05-21 16:06:39 +0200

    Tried doing the exercise in environment 02

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+//Del 1
+function start() {
+    document
+        .querySelector("#create-form")
+        .addEventListener("submit", newAnimal);
+}
+
+function newAnimal(event) {
+    const animalArr = [];
+    console.log(animalArr);
+    event.preventDefault();
+    const form = event.target;
+
+    //Del 2
+    const newAnimalObj = {
+        name: form.animalName.value,
+        animalType: form.animalType.value,
+        age: form.animalAge.value,
+    };
+
+    animalArr.push(newAnimalObj);
+    return newAnimalObj;
+}
+
+//Del 3
