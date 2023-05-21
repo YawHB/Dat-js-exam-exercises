e2fc8cf35683e342c5c47f8e34c064b2368dd832
YawHB <yawhboateng@gmail.com>
2023-05-21 18:57:18 +0200

    Finished the exercise

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -2,28 +2,67 @@
 
 window.addEventListener("load", start);
 
-//Del 1
 function start() {
     document
         .querySelector("#create-form")
-        .addEventListener("submit", newAnimal);
+        .addEventListener("submit", userInputForm);
 }
 
-function newAnimal(event) {
-    const animalArr = [];
-    console.log(animalArr);
+//Del 1
+
+function newAnimalObj(name, type, age) {
+    return {
+        name,
+        type,
+        age,
+    };
+}
+
+//Del 2
+
+function userInputForm(event) {
     event.preventDefault();
+
     const form = event.target;
 
-    //Del 2
-    const newAnimalObj = {
-        name: form.animalName.value,
-        animalType: form.animalType.value,
-        age: form.animalAge.value,
-    };
+    const name = form.animalName.value;
+    const type = form.animalType.value;
+    const age = form.animalAge.value;
 
-    animalArr.push(newAnimalObj);
-    return newAnimalObj;
+    const animalCreated = newAnimalObj(name, type, age);
+    allAnimals.push(animalCreated);
+    clearTableContent();
+    sortAnimals();
 }
 
+let allAnimals = [];
+
 //Del 3
+
+function clearTableContent() {
+    const tableBody = document.querySelector("#list-container table tbody");
+    tableBody.innerHTML = "";
+}
+
+function sortAnimals() {
+    const sortedByNAme = allAnimals.sort(sortByFirstName);
+    console.log(sortedByNAme);
+    sortedByNAme.forEach(displayAnimals);
+}
+
+function sortByFirstName(animal1, animal2) {
+    return animal1.name.localeCompare(animal2.name);
+}
+
+function displayAnimals(animal) {
+    const html = /*html*/ `
+
+    <td id="names-in-table">${animal.name}</td>
+    <td id="type-in-table"  >${animal.type}</td>
+    <td id="age-in-table" >${animal.age}</td>
+    `;
+
+    document
+        .querySelector("#list-container table tbody")
+        .insertAdjacentHTML("beforeend", html);
+}
