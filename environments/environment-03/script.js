9053860e78a8634e0b2f0d36b1194f02d43e9be5
YawHB <yawhboateng@gmail.com>
2023-05-21 16:07:28 +0200

    practiced fetching from another file

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+async function start() {
+    const data = await getData();
+    console.log(data);
+}
+
+async function getData() {
+    const response = await fetch("users.json");
+    console.log(response);
+    const data = response.json();
+    return data;
+}
