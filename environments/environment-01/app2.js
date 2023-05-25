e106e246d334aafeb314b42e7a2832eb4facb28d
YawHB <yawhboateng@gmail.com>
2023-05-25 10:40:32 +0200

    Added exercise 4

--- environments/environment-01/app2.js
+++ environments/environment-01/app2.js
@@ -1,57 +1,57 @@
-"use strict";
+// "use strict";
 
-window.addEventListener("load", start);
+// window.addEventListener("load", start);
 
-let userArr = [];
+// let userArr = [];
 
-function start() {
-    getData();
+// function start() {
+//     getData();
 
-    const newObj = addNewObj("Hans", true, "admin");
-    userArr.push(newObj);
-}
+//     const newObj = addNewObj("Hans", true, "admin");
+//     userArr.push(newObj);
+// }
 
-async function getData() {
-    const res = await fetch("./users.json");
-    const data = await res.json();
+// async function getData() {
+//     const res = await fetch("./users.json");
+//     const data = await res.json();
 
-    userArr = pushUsersToArray(data);
-    showUsers(userArr);
-    console.log(userArr);
-}
+//     userArr = pushUsersToArray(data);
+//     showUsers(userArr);
+//     console.log(userArr);
+// }
 
-function pushUsersToArray(users) {
-    for (const user of users) {
-        userArr.push(user);
-    }
-    return userArr;
-}
+// function pushUsersToArray(users) {
+//     for (const user of users) {
+//         userArr.push(user);
+//     }
+//     return userArr;
+// }
 
-function showUsers(users) {
-    //* Display active users using for of loop
-    for (const user of users) {
-        if (user.active) {
-            showUser(user);
-        }
-    }
+// function showUsers(users) {
+//     //* Display active users using for of loop
+//     for (const user of users) {
+//         if (user.active) {
+//             showUser(user);
+//         }
+//     }
 
-    //*Display active users using filter
-    // const activeUsers = users.filter((user) => user.active);
-    // showUser(activeUsers);
-}
+//     //*Display active users using filter
+//     // const activeUsers = users.filter((user) => user.active);
+//     // showUser(activeUsers);
+// }
 
-function showUser(user) {
-    const html = /*html*/ `
-    <li>${user.name} - is active</li>
-    
-    `;
-    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
-}
+// function showUser(user) {
+//     const html = /*html*/ `
+//     <li>${user.name} - is active</li>
 
-function addNewObj(name, active, role) {
-    return {
-        name,
-        active,
-        role,
-    };
-}
+//     `;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+// }
+
+// function addNewObj(name, active, role) {
+//     return {
+//         name,
+//         active,
+//         role,
+//     };
+// }
