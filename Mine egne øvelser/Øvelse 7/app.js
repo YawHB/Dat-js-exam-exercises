facc8382b47a4bc70ab39fa0272326c4ba904440
YawHB <yawhboateng@gmail.com>
2023-05-29 18:15:02 +0200

    Completed øvelse 7 again, but without fetching

--- Mine egne øvelser/Øvelse 7/app.js
+++ Mine egne øvelser/Øvelse 7/app.js
@@ -1,26 +1,86 @@
 "use strict";
 
-const poepleArr = [];
+// const poepleArr = [];
+
+// window.addEventListener("load", start);
+
+// async function start() {
+//     const data = await fetchData();
+
+//     document
+//         .querySelector("#personForm")
+//         .addEventListener("submit", submitForm);
+// }
+
+// async function fetchData() {
+//     const res = await fetch("./people.json");
+//     const data = await res.json();
+//     data.forEach(pushToArr);
+//     console.log(poepleArr);
+// }
+
+// function pushToArr(person) {
+//     poepleArr.push(person);
+// }
+
+// function submitForm(event) {
+//     event.preventDefault();
+
+//     const form = event.target;
+
+//     // const name = form.name.value;
+//     // const age = form.age.value;
+//     // const male = form.male.checked;
+//     // const female = form.female.checked;
+
+//     // const newPerson = addPerson(name, age, male, female);
+
+//     const newPerson = {
+//         name: form.name.value,
+//         age: form.age.value,
+//         male: form.male.checked,
+//         female: form.female.checked,
+//     };
+
+//     poepleArr.push(newPerson);
+//     const sortedAges = sortedBy(poepleArr);
+//     showPersons(sortedAges);
+// }
+
+// function sortedBy(people) {
+//     return people.sort((a, b) => a.age - b.age);
+// }
+
+// // function addPerson(name, age, male, female) {
+// //     return {
+// //         name,
+// //         age,
+// //         male,
+// //         female,
+// //     };
+// // }
+
+// function showPersons(persons) {
+//     document.querySelector("ul").innerHTML = "";
+//     for (const person of persons) display(person);
+// }
+
+// function display(person) {
+//     const html = /*html*/ `
+//     <li>${person.name} - ${person.age} </li>
+
+//     `;
+
+//     document.querySelector("ul").insertAdjacentHTML("beforeend", html);
+// }
 
 window.addEventListener("load", start);
 
-async function start() {
-    const data = await fetchData();
-
+const peopleArr = [];
+function start() {
     document
         .querySelector("#personForm")
         .addEventListener("submit", submitForm);
-}
-
-async function fetchData() {
-    const res = await fetch("./people.json");
-    const data = await res.json();
-    data.forEach(pushToArr);
-    console.log(poepleArr);
-}
-
-function pushToArr(person) {
-    poepleArr.push(person);
 }
 
 function submitForm(event) {
@@ -28,49 +88,43 @@ function submitForm(event) {
 
     const form = event.target;
 
-    // const name = form.name.value;
-    // const age = form.age.value;
-    // const male = form.male.checked;
-    // const female = form.female.checked;
+    const name = form.name.value;
+    const age = form.age.value;
+    const male = form.male.checked;
+    const female = form.female.checked;
 
-    // const newPerson = addPerson(name, age, male, female);
+    const newPerson = addPerson(name, age, male, female);
+    peopleArr.push(newPerson);
+    console.log(peopleArr);
 
-    const newPerson = {
-        name: form.name.value,
-        age: form.age.value,
-        male: form.male.checked,
-        female: form.female.checked,
-    };
-
-    poepleArr.push(newPerson);
-    const sortedAges = sortedBy(poepleArr);
-    showPersons(sortedAges);
+    const sortedBy = sortBy(peopleArr);
+    showPeople(sortedBy);
 }
 
-function sortedBy(people) {
+function sortBy(people) {
     return people.sort((a, b) => a.age - b.age);
 }
 
-// function addPerson(name, age, male, female) {
-//     return {
-//         name,
-//         age,
-//         male,
-//         female,
-//     };
-// }
-
-function showPersons(persons) {
+function showPeople(people) {
     document.querySelector("ul").innerHTML = "";
-    for (const person of persons) display(person);
+    for (const person of people) {
+        display(person);
+    }
 }
 
 function display(person) {
     const html = /*html*/ `
-    <li>${person.name} - ${person.age} </li>
     
-    
-    `;
+    <li>${person.name} ${person.age}</li>
 
+    `;
     document.querySelector("ul").insertAdjacentHTML("beforeend", html);
+}
+
+function addPerson(name, age, gender) {
+    return {
+        name,
+        age,
+        gender,
+    };
 }
