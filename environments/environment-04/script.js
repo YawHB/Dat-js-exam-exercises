3d103610538d79225c74e36327ca44632023ec11
YawHB <yawhboateng@gmail.com>
2023-05-31 20:39:33 +0200

    Completed Øvelse 9.1

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,34 @@
-"use strict";
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start() {
+    sortName("name");
+    // sortEmail();
+}
+
+function sortName(sortBy) {
+    teachers.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+    showTeachers();
+}
+
+function sortEmail() {
+    teachers.sort((a, b) => a.email.localeCompare(b.email));
+    showTeachers();
+}
+
+function showTeachers() {
+    for (const teacher of teachers) {
+        const html = /*html*/ `
+        
+
+        
+        <li>${teacher.name} - ${teacher.email}</li>
+        
+        `;
+
+        document
+            .querySelector("#teachers-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
