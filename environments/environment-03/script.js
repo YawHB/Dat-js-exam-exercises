76f9d3569c46ae81824f97d3eda3291c9d2c41ef
YawHB <yawhboateng@gmail.com>
2023-05-31 16:09:59 +0200

    Complete exercise 6.1

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -2,14 +2,48 @@
 
 window.addEventListener("load", start);
 
-async function start() {
-    const data = await getData();
-    console.log(data);
+const products = [
+    { name: "phone", price: 100, inStock: true },
+    { name: "Book", price: 20, inStock: false },
+    { name: "Bike", price: 50, inStock: true },
+];
+
+function start() {
+    document
+        .querySelector("#create-form")
+        .addEventListener("submit", submitForm);
+    showPorducts();
 }
 
-async function getData() {
-    const response = await fetch("users.json");
-    console.log(response);
-    const data = response.json();
-    return data;
+function showPorducts() {
+    const productsInStock = products.filter((product) => product.inStock);
+    document.querySelector("#list-container").innerHTML = "";
+
+    for (const product of productsInStock) {
+        const html = /*html*/ `
+        
+        <p>${product.name} $${product.price} </p>
+        
+        `;
+
+        document
+            .querySelector("#list-container")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const newProduct = {
+        name: form.name.value,
+        price: form.price.value,
+        inStock: form.inStock.checked,
+    };
+
+    products.push(newProduct);
+    console.log(products);
+    showPorducts();
 }
