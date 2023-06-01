af999b39c7e04fa6abe0551a9ceddc6dffb2cba4
YawHB <yawhboateng@gmail.com>
2023-06-01 12:38:24 +0200

    Made Øvelse 12.1

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,21 @@
+import { courses } from "./courses.js";
 
+window.addEventListener("load", start);
+
+function start() {
+    courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+    courses.reverse();
+    showCourses();
+}
+
+function showCourses() {
+    for (const course of courses) {
+        const html = /*html*/ `
+        
+        <li>${course.name} - ${course.ectsPoints} - ${course.teacher}</li>
+        `;
+        document
+            .querySelector("#courses-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
