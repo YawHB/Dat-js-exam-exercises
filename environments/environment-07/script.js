e94dd9da0fb22e9e80939dac47c6d7479248195b
YawHB <yawhboateng@gmail.com>
2023-06-02 12:16:37 +0200

    Completed  Øvelse 21.1

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,73 +1,8 @@
-// "use strict";
-
-// window.addEventListener("load", start);
-
-// let students = [];
-
-// function start() {
-//     console.log();
-
-//     document
-//         .querySelector("#create-student-form")
-//         .addEventListener("submit", submitForm);
-// }
-
-// function submitForm(event) {
-//     event.preventDefault();
-
-//     const form = event.target;
-
-//     const name = form.name.value;
-//     const email = form.email.value;
-//     const age = Number(form.age.value);
-
-//     const newStudent = addStudent(name, email, age);
-//     console.log(newStudent);
-//     students.push(newStudent);
-//     sortby(students);
-//     showStudents(sortedByAge);
-// }
-
-// function showStudents(students) {
-//     document.querySelector("tbody").innerHTML = "";
-//     for (const student of students) {
-//         displayStudent(student);
-//     }
-// }
-
-// function sortby(array) {
-//     array.sort((a, b) => a.age - b.age);
-// }
-
-// function displayStudent(student) {
-//     const html = /*html*/ `
-
-//     <tr>
-
-//     <td>${student.name}</td>
-//     <td>${student.email}</td>
-//     <td>${student.age}</td>
-
-//     </tr>
-
-//     `;
-
-//     document.querySelector("tbody").insertAdjacentHTML("beforeend", html);
-// }
-
-// function addStudent(name, email, age) {
-//     return {
-//         name,
-//         email,
-//         age,
-//     };
-// }
-
 "use strict";
 
 window.addEventListener("load", start);
 
-const studentsArr = [];
+let students = [];
 
 function start() {
     document
@@ -80,48 +15,30 @@ function submitForm(event) {
 
     const form = event.target;
 
-    const name = form.name.value;
-    const email = form.email.value;
-    const age = Number(form.age.value);
-
-    createStudent(name, email, age);
-}
-
-function createStudent(name, email, age) {
     const newStudent = {
-        name,
-        email,
-        age,
+        name: form.name.value,
+        email: form.email.value,
+        age: Number(form.age.value),
     };
 
-    studentsArr.push(newStudent);
-    console.log(studentsArr);
-
-    const filteredByAge = studentsArr.filter((student) => student.age >= 18);
-
-    filteredByAge.sort((a, b) => a.name.localeCompare(b.name));
-
-    showStudents(filteredByAge);
+    students.push(newStudent);
+    students = students.filter((student) => student.age >= 18);
+    students.sort((a, b) => a.name.localeCompare(b.name));
+    showStudent();
 }
 
-function showStudents(array) {
+function showStudent() {
     document.querySelector("tbody").innerHTML = "";
-    for (const student of array) {
-        displayStudent(student);
+    for (const student of students) {
+        const html = /*html*/ `
+        
+        <tr>
+            <td>${student.name} - ${student.email} - ${student.age}</td>
+        </tr>
+        
+        `;
+        document
+            .querySelector("#students-table-body")
+            .insertAdjacentHTML("beforeend", html);
     }
-}
-
-function displayStudent(student) {
-    const html = /*html*/ `
-    
-    <tr>
-
-<td>${student.name} - ${student.age}</td>
-
-
-    </tr>
-
-    `;
-
-    document.querySelector("tbody").insertAdjacentHTML("beforeend", html);
 }
