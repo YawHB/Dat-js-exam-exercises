0045e897d02913a03fe9569b07f19b7030dbb608
YawHB <yawhboateng@gmail.com>
2023-06-02 18:22:59 +0200

    Completed Øvelse 10.2

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,31 @@
-"use strict";
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start() {
+    showTeachers();
+    addTeacher("Peter", "petl.kea.dk");
+    addTeacher("Rasmus", "race.kea.dk");
+}
+
+function showTeachers() {
+    document.querySelector("#teachers-list").innerHTML = "";
+    for (const teacher of teachers) {
+        const html = /*html*/ `
+        <li>${teacher.name} - ${teacher.email}</li>
+        
+        `;
+        document
+            .querySelector("#teachers-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function addTeacher(name, email) {
+    const newTeacher = {
+        name,
+        email,
+    };
+    teachers.push(newTeacher);
+    showTeachers();
+}
