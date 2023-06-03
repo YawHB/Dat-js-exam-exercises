20615b8d650f74c58d80aefa1422c3718fd6cba0
YawHB <yawhboateng@gmail.com>
2023-06-03 15:28:26 +0200

    Completed Øvelse 11.2

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,35 @@
+import { courses } from "./courses.js";
 
+window.addEventListener("load", start);
+
+function start() {
+    showCourses();
+
+    addCourse("JS coding", 5, "Peter Lind");
+    addCourse("UX ", 5, "Rasmus Cederdoorff");
+}
+
+function showCourses() {
+    document.querySelector("#courses-list").innerHTML = "";
+    for (const course of courses) {
+        const html = /*html*/ `
+        
+        <li>${course.name} - ${course.ectsPoints} - ${course.teacher}</li>
+        
+        `;
+        document
+            .querySelector("#courses-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function addCourse(name, ectsPoints, teacher) {
+    const newCourse = {
+        name,
+        ectsPoints,
+        teacher,
+    };
+
+    courses.push(newCourse);
+    showCourses();
+}
