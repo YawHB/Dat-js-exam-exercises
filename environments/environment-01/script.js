5263bbad74c7947d5f5a0e711497d004eb347339
YawHB <yawhboateng@gmail.com>
2023-06-03 14:20:13 +0200

    Completed Øvelse 1.1

---
+++ environments/environment-01/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+window.addEventListener("load", start);
+
+let users = [];
+async function start() {
+    users = await fetchUsers();
+    console.log(users);
+    users = users.filter((user) => user.role === "admin");
+    showUsers();
+}
+
+async function fetchUsers() {
+    const res = await fetch("users.json");
+    return await res.json();
+}
+
+function showUsers() {
+    for (const user of users) {
+        const html = /*html*/ `
+        
+        <li>${user.name} - ${user.active ? "Active" : "Not active"}</li>
+        
+        `;
+
+        document
+            .querySelector("#userlist")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
