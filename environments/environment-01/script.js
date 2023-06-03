c888f91a4cfa5486c6537a26cae281be3f4a7a58
YawHB <yawhboateng@gmail.com>
2023-06-03 16:30:49 +0200

    Completed Øvelse 02.1

---
+++ environments/environment-01/script.js
@@ -0,0 +1,43 @@
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start() {
+    users = await fetchUsers();
+    console.log(users);
+
+    showUsers();
+}
+
+async function fetchUsers() {
+    const res = await fetch("users.json");
+    return await res.json();
+}
+
+function showUsers() {
+    let admin = 0;
+    let user = 0;
+    let guest = 0;
+    for (const user of users) {
+        if (user.role === "admin") {
+            admin++;
+        } else if (user.role === "user") {
+            user++;
+        } else if (user.role === "guest") {
+            guest++;
+        }
+
+        const html = /*html*/ `
+        
+        <li>${user.name} - ${user.role} - ${user.active}</li>
+        
+        `;
+        document
+            .querySelector("#userlist")
+            .insertAdjacentHTML("beforeend", html);
+    }
+
+    document.querySelector("#admin-count").textContent = admin;
+    document.querySelector("#user-count").textContent = user;
+    document.querySelector("#guest-count").textContent = guest;
+}
