5136d16454f0b88292a1d1d5a5b95c163ade3017
YawHB <yawhboateng@gmail.com>
2023-06-04 11:02:39 +0200

    Completed Øvelse 12.2

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,21 @@
+import { courses } from "./courses.js";
 
+window.addEventListener("load", start);
+
+function start() {
+    courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+    showCourses();
+}
+
+function showCourses() {
+    for (const course of courses) {
+        const html = /*html*/ `
+        
+        <li>${course.name} - ${course.ectsPoints} </li>
+        
+        `;
+        document
+            .querySelector("#courses-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
