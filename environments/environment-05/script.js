8e990980cc6751f8c1e5a5e8505de9c1e7f61df6
YawHB <yawhboateng@gmail.com>
2023-06-04 12:48:20 +0200

    Completed Øvelse 13.3

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,39 @@
+import { courses } from "./courses.js";
 
+window.addEventListener("load", start);
+
+function start() {
+    console.log(courses);
+    showCourses(courses);
+    document
+        .querySelector("#select-filter-ects")
+        .addEventListener("change", filterBy);
+}
+
+function showCourses(courses) {
+    document.querySelector("#courses-list").innerHTML = "";
+    for (const course of courses) {
+        const html = /*html*/ `
+        
+        <li>${course.name} - ${course.ectsPoints} </li>
+        `;
+        document
+            .querySelector("#courses-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function filterBy(event) {
+    let selected = event.target.value;
+    console.log(typeof selected);
+
+    let filteredCourses = courses.filter(
+        (course) => course.ectsPoints === Number(selected)
+    );
+
+    if (filteredCourses.length === 0) {
+        document.querySelector("h2").textContent = "No courses available";
+    }
+    console.log(filteredCourses);
+    showCourses(filteredCourses);
+}
