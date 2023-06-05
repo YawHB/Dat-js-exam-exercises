79bd98bdcf22f52e817ae492b66c15020d377e91
YawHB <yawhboateng@gmail.com>
2023-06-05 15:25:04 +0200

    Completed Øvelse 22.1

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let playlist = [];
+
+async function start() {
+    playlist = await fetchSongs();
+    console.log(playlist);
+    showSongs();
+}
+
+async function fetchSongs() {
+    const res = await fetch("playlist.json");
+    return res.json();
+}
+
+function showSongs() {
+    document.querySelector("#songlist").innerHTML = "";
+    for (const song of playlist) {
+        const html = /*html*/ `
+        
+        <li> ${song.artist} - ${song.title} - (${song.duration}) <button>Remove</button>
+        </li>
+
+        `;
+        document
+            .querySelector("#songlist")
+            .insertAdjacentHTML("beforeend", html);
+
+        document
+            .querySelector("#songlist li:last-child button")
+            .addEventListener("click", () => removeSong(song));
+    }
+}
+
+function removeSong(song) {
+    const index = playlist.indexOf(song);
+
+    playlist.splice(index, 1);
+    showSongs();
+}
